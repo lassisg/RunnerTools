eec1c159eceda13c128abf0faba7840ea057fdbd
Leandro Gabriel <lgabriel@growin.com>
2023-03-24 17:21:14 +0000

    Add Detail page for workouts

---
+++ src/Application/Workouts/Queries/GetWorkouts/GetWorkoutDetailQuery.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using RunnerTools.Application.Common.Exceptions;
+using RunnerTools.Application.Common.Interfaces;
+using RunnerTools.Domain.Entities;
+
+namespace RunnerTools.Application.Workouts.Queries.GetWorkouts;
+
+public record GetWorkoutDetailQuery(int Id) : IRequest<WorkoutVm>;
+
+public class GetWorkoutDetailQueryHandler : IRequestHandler<GetWorkoutDetailQuery, WorkoutVm>
+{
+    private readonly IApplicationDbContext _context;
+    private readonly IMapper _mapper;
+
+    public GetWorkoutDetailQueryHandler(IApplicationDbContext context, IMapper mapper)
+    {
+        _context = context;
+        _mapper = mapper;
+    }
+
+    public async Task<WorkoutVm> Handle(GetWorkoutDetailQuery request, CancellationToken cancellationToken)
+    {
+        var entity = await _context.Workouts
+                                   .ProjectTo<WorkoutVm>(_mapper.ConfigurationProvider)
+                                   .SingleOrDefaultAsync(w => w.Id == request.Id, cancellationToken);
+
+        if (entity == null)
+            throw new NotFoundException(nameof(Workout), request.Id);
+        
+        return entity;
+    }
+}
---
+++ src/WebUI/Pages/Workouts/Details.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using RunnerTools.Application.Activities.Queries.GetActivities;
+using RunnerTools.Application.Workouts.Queries.GetWorkouts;
+
+namespace WebUI.Pages.Workouts;
+
+public class Details : PageModel
+{
+    private readonly IMediator _mediator;
+
+    public Details(IMediator mediator) => _mediator = mediator;
+
+    public WorkoutVm Data { get; private set; }
+
+    public async Task OnGetAsync(GetWorkoutDetailQuery query) => 
+        Data = await _mediator.Send(query);
+}
