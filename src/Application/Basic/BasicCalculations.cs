e0b1733d89acc012847076e7439a181402265dd6
Leandro Gabriel <lgabriel@growin.com>
2023-03-28 18:04:42 +0100

    Add test for speed to cadence calculation

---
+++ src/Application/Basic/BasicCalculations.cs
@@ -0,0 +1,16 @@
+﻿namespace RunnerTools.Application.Basic;
+
+public static class BasicCalculations
+{
+    private const int MinutesInHour = 60;
+    private const int SecondsInMinute = 60;
+    
+    public static TimeSpan ToCadence(this decimal speed)
+    {
+        var cadenceNumeric = MinutesInHour / speed;
+        var cadenceMinutes = (int)cadenceNumeric;
+        var cadenceSeconds = (int)((cadenceNumeric - cadenceMinutes) * SecondsInMinute);
+        
+        return new TimeSpan(0, cadenceMinutes, cadenceSeconds);
+    }
+}
---
+++ tests/Application.UnitTests/Basic/BasicCalculationsTests.cs
@@ -0,0 +1,22 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using RunnerTools.Application.Basic;
+
+namespace RunnerTools.Application.UnitTests.Basic;
+
+[TestFixture]
+public class BasicCalculationsTests
+{
+    [Test]
+    [TestCase(10, 6, 0)]
+    [TestCase(10.43, 5, 45)]
+    [TestCase(11, 5, 27)]
+    [TestCase(13.33, 4, 30)]
+    public void ShouldReturnCorrectCadence(decimal speed, int minutes, int seconds)
+    {
+        var cadence = new TimeSpan(0, minutes, seconds);
+        var result = speed.ToCadence();
+
+        result.Should().Be(cadence);
+    }
+}
