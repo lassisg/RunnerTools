b91838b1886a69a12ed3f72a824ae27c9b951c4b
Leandro Gabriel <lgabriel@growin.com>
2023-03-29 09:41:27 +0100

    Add test for cadence to speed calculation

--- src/Application/Basic/BasicCalculations.cs
+++ src/Application/Basic/BasicCalculations.cs
@@ -13,4 +13,18 @@ public static class BasicCalculations
         
         return new TimeSpan(0, cadenceMinutes, cadenceSeconds);
     }
+    
+    public static decimal ToSpeed(this TimeSpan cadence)
+    {
+        var cadenceMinutes = cadence.Minutes;
+        var cadenceSeconds = cadence.Seconds;
+        decimal cadenceNumeric = cadenceMinutes + (decimal)cadenceSeconds/SecondsInMinute;
+        decimal speed = MinutesInHour / cadenceNumeric;
+        
+        return Decimal.Round(speed,2);
+    }
+}
+
+public class Timespan
+{
 }
--- tests/Application.UnitTests/Basic/BasicCalculationsTests.cs
+++ tests/Application.UnitTests/Basic/BasicCalculationsTests.cs
@@ -19,4 +19,29 @@ public class BasicCalculationsTests
 
         result.Should().Be(cadence);
     }
+    
+    [Test]
+    [TestCase(6, 0, 10)]
+    [TestCase(5, 45, 10.43)]
+    [TestCase(5, 27, 11)]
+    [TestCase(4, 30, 13.33)]
+    public void ShouldReturnCorrectSpeed(int minutes, int seconds, decimal speed)
+    {
+        var cadence = new TimeSpan(0, minutes, seconds);
+        var result = cadence.ToSpeed();
+
+        result.Should().BeApproximately(speed,0.01M);
+    }
+    /* 
+     * Cálculo de ritmo (dada distância e tempo desejados)
+     * Exemplo: 10 km em 50 minutos -> 5 min/km
+     */ 
+    /* 
+     * Cálculo de velocidade (dada uma distância e tempo desejados)
+     * Exemplo: 10 km em 50 minutos -> 12 min/km
+     */
+    /* 
+     * Cálculo de tempo (dados ritmo e distância desejados)
+     * Exemplo: 21,097 km (meia maratona) com ritmo de 5:30 min/km -> 1:56:02
+     */
 }
