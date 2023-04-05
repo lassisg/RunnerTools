7bc67eaa62cee8d801d32e0ad78e18f92a94fc24
Leandro Gabriel <lgabriel@growin.com>
2023-04-05 15:20:38 +0100

    Add missing migrations to VCS

---
+++ src/Infrastructure/Persistence/Migrations/20230323112953_AddActivityName.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RunnerTools.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddActivityName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Activities",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Activities");
+        }
+    }
+}
